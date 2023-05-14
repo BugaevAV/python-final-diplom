bf9b9374230fdc8c4f0f1ce6ecb3e1f9bd23b08c
Alexander Bugaev <iustusvir@gmail.com>
2023-05-15 00:13:18 +0300

    added drf_spectacular routes

--- orders/orders/urls.py
+++ orders/orders/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('users_auth.urls')),
     path('api/v1/', include('create_orders.urls')),
-    path('api/v1/shop/', include('ordering_goods.urls'))
+    path('api/v1/shop/', include('ordering_goods.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
